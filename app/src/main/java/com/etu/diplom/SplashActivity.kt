951f8873be2b8ea1bd25bc5e727d9c111a707f90
IceFroggit <zeksengali0@gmail.com>
2024-05-07 20:35:04 +0300

    ui design

---
+++ app/src/main/java/com/etu/diplom/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.etu.diplom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
